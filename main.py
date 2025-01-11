13b954b1735368b0fa770d81ad07cc177641f90d
Muhid <158451729+Muhid-Cyber@users.noreply.github.com>
2025-01-12 02:00:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def is_valid_youtube_url(url):
         return parsed_url.path[1:] if parsed_url.netloc == 'youtu.be' else False
     return False
 
+
 @app.route('/audio', methods=['GET'])
 def get_audio_info():
     url = request.args.get('url')
@@ -19,18 +20,32 @@ def get_audio_info():
         return jsonify({"error": "Missing 'url' parameter."}), 400
     if not is_valid_youtube_url(url):
         return jsonify({"error": "Invalid Youtube link format."}), 400
+
     try:
-        with yt_dlp.YoutubeDL({'quiet': True, 'format': 'bestaudio', 'extractaudio': True,'nocheckcertificate':True}) as ydl:
-          info_dict = ydl.extract_info(url, download=False)
-          if info_dict:
-            return jsonify({"title": info_dict.get('title', "Unknown Title"),"best_audio_url":info_dict.get('url', "N/A")}), 200
-          return jsonify({"error": "Video not found"}), 404
+        ydl_opts = {
+            'quiet': True,
+            'format': 'bestaudio',
+            'extractaudio': True,
+             'nocheckcertificate':True,
+             'cookiefile': 'cookies.txt'
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            if info_dict:
+                return jsonify({
+                    "title": info_dict.get('title', "Unknown Title"),
+                    "best_audio_url": info_dict.get('url', "N/A")
+                }), 200
+            return jsonify({"error": "Video not found"}), 404
+
     except yt_dlp.utils.DownloadError as e:
-       if 'This video is unavailable' in str(e) :
-          return jsonify({"error": "Video is unavailable or deleted"}), 404
-       if  'Please check your internet connection or try again in a few minutes' in str(e):
-             return jsonify({"error":"Temporary Server Error"}), 503
-       return jsonify({"error": f"An unexpected error happened: {str(e)}"}), 500
+        if 'This video is unavailable' in str(e):
+            return jsonify({"error": "Video is unavailable or deleted"}), 404
+        if 'Please check your internet connection or try again in a few minutes' in str(e):
+            return jsonify({"error": "Temporary Server Error"}), 503
+        return jsonify({"error": f"An unexpected error happened: {str(e)}"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000, host='0.0.0.0')
